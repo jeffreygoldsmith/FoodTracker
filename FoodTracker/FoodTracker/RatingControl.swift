effcbc391cde7be95d6db8f499d36e1fd293661f
Jeffrey Goldsmith <jgoldsmith@rsgc.on.ca>
2016-11-05 18:11:13 -0400

    Added rating control

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -11,16 +11,30 @@ import UIKit
 class RatingControl: UIView {
     // MARK: Properties
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
     var ratingButtons = [UIButton]()
+    let spacing = 10
+    let starCount = 5
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-            button.backgroundColor = UIColor.red
+        let filledStarImage = UIImage(named: "filledStar")
+        let emptyStarImage = UIImage(named: "emptyStar")
+        
+        for _ in 0..<starCount {
+            let button = UIButton()
+            
+            button.setImage(emptyStarImage, for: .normal)
+            button.setImage(filledStarImage, for: .selected)
+            button.setImage(filledStarImage, for: [.highlighted, .selected])
+            
+            button.adjustsImageWhenHighlighted = false
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
             ratingButtons += [button]
             addSubview(button)
@@ -28,23 +42,37 @@ class RatingControl: UIView {
     }
     
     override func layoutSubviews() {
+        // Using the tutorial's code causes the buttons to be abnormally large
         var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
         
-        // Offset each button's origin by the length of the button plus spacing.
+        // Offset each button's origin by the length of the button plus some spacing.
         for (index, button) in ratingButtons.enumerated() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (44 + spacing))
             button.frame = buttonFrame
         }
+        
+        updateButtonSelectionStates()
     }
-    
+
     override public var intrinsicContentSize: CGSize {
         get {
-            return CGSize(width: 240, height: 44)
+            let buttonSize = Int(frame.size.height)
+            let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+            
+            return CGSize(width: width, height: buttonSize)
         }
     }
     
     // MARK: Button Action
     func ratingButtonTapped(_ button: UIButton) {
-        print("Button pressed 👍")
+        rating = ratingButtons.index(of: button)! + 1
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
     }
 }
