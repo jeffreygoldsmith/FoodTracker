69ebc726119f9e6a39abd25a46e4d8507654fd57
Jeffrey Goldsmith <jgoldsmith@rsgc.on.ca>
2016-11-05 16:36:14 -0400

    Added button

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -12,5 +12,21 @@ class RatingControl: UIView {
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), for: .touchDown)
+        addSubview(button)
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        get {
+            return CGSize(width: 240, height: 44)
+        }
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(_ button: UIButton) {
+        print("Button pressed 👍")
     }
 }
